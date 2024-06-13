db936b57b850085f3a8c1319c9cc54356e3041ed
Royce Hoe <royce.hoe@gofluid.io>
2024-06-13 19:17:42 +0800

    feat: Create shortcut for pushing to git repo; change shortcut for viewing harpoon

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,13 +1,14 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- [p]roject [v]iew
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git) --[g]et [s]tatus
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) --[g]et [s]tatus
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git) --[g]it [s]tatus
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>") --[g]it [p]ush
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) --[u]ndo tree toggle
 
 vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help) --[s]ignature
 vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help) --[s]ignature
 
-vim.keymap.set("n", "<leader>h", function() -- [e]xplore files with harpoon
+vim.keymap.set("n", "<leader>f", function() -- [f]iles stored in harpoon
     require("harpoon.ui").toggle_quick_menu()
 end
 )

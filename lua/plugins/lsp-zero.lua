b4b85e6cbe3acee940703519f31f33171af3e5bf
Royce Hoe <royce.hoe@gofluid.io>
2024-06-16 15:16:45 +0800

    feat: implement snippet autocompletion

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -8,29 +8,48 @@ return {
         lazy = false,
         config = true,
     },
-
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets",
+            "saadparwaiz1/cmp_luasnip"
+        },
+    },
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            "L3MON4D3/LuaSnip"
+            "L3MON4D3/LuaSnip",
+            "rafamadriz/friendly-snippets"
         },
         config = function()
             -- Here is where you configure the autocompletion settings.
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_cmp()
+            require("luasnip.loaders.from_vscode").lazy_load()
 
             local cmp = require('cmp')
             local cmp_select = {behavior = cmp.SelectBehavior.Select}
-            local cmp_mappings = cmp.mapping.preset.insert({
-                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            })
+
             cmp.setup({
-                mapping = cmp_mappings
+                snippet = {
+                    expand = function (args)
+                        require('luasnip').lsp_expand(args.body)
+                    end
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'luasnip'}
+                })
             })
         end
     },
